eb83486f636eaec903054371bf1e744d01314361
Habel <tamikys15@gmail.com>
2022-07-04 17:37:33 +0300

    Player script and controller interface creating

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Awake()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        
+    }
+    
+}
