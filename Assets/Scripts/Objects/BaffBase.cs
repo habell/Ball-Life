4bd8bf85ababe99ff1ca0bf8356cd3b100c19f9b
Habel <tamikys15@gmail.com>
2022-07-07 20:56:30 +0300

    Interactive object base

---
+++ Assets/Scripts/Objects/BaffBase.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace.Abstract;
+using UnityEngine;
+
+public class BaffBase : InteractiveObject
+{
+    protected override void TriggerAccept(GameObject obj)
+    {
+        throw new System.NotImplementedException();
+    }
+}
