640d79edf077a7313454942fb6d83a2271090b6d
Habel <tamikys15@gmail.com>
2022-07-08 02:31:29 +0300

    health, baff health effect

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private float _value;
+
+    public float Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            if (_value <= 0)
+            {
+                Death();
+            }
+        }
+    }
+
+    public void AddHealth(float extraHealth)
+    {
+        _value += extraHealth;
+    }
+    
+    private void Death()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/Objects/BaffBase.cs
+++ Assets/Scripts/Objects/BaffBase.cs
@@ -7,9 +7,44 @@ using UnityEngine;
 public class BaffBase : InteractiveObject
 {
     [SerializeField]
-    private BaffType _baffType;
+    private BaffType _type;
+
+    [SerializeField]
+    private float _value;
+
+    [SerializeField]
+    private bool _reusable;
+
     protected override void TriggerAccept(GameObject obj)
     {
-        throw new System.NotImplementedException();
+        Use(obj);
+        if (_reusable)
+        {
+            RemoveBaff();
+        }
+    }
+
+
+    private void Use(GameObject obj)
+    {
+        switch (_type)
+        {
+            case BaffType.ExtraHealth:
+                break;
+            case BaffType.ExtraSpeed:
+                break;
+            default: break;
+        }
+    }
+
+    private void ExtraHealth(GameObject obj)
+    {
+        obj.GetComponent<Health>().AddHealth(_value);
+    }
+
+    private void RemoveBaff()
+    {
+        print("Baff Removed!");
+        Destroy(gameObject);
     }
 }
